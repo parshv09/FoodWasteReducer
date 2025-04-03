5bcccfba08475046188c120396f468d068b13146
Parshv Athane <parshvrajkumarathane@gmail.com>
2025-04-03 12:18:29 +0530

    Ui updated and some inventory database changes

--- FoodWasteReducer/inventory/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'inventory'
-
-urlpatterns = [
-    path('dashboard', views.inventory_dashboard, name='dashboard'),
-    path('add/', views.add_food_item, name='add_food'),
-]
