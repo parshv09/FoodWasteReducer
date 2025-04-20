4599076b98ac4c1d02eb97b1f6eeae94798e950b
Parshv Athane <parshvrajkumarathane@gmail.com>
2025-04-20 23:51:05 +0530

    adding save recipe

--- FoodWasteReducer/recipes/models.py
+++ FoodWasteReducer/recipes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
@@ -8,3 +9,17 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
+
+class SavedRecipe(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe_id = models.CharField(max_length=100)  # External API ID
+    title = models.CharField(max_length=255)
+    image_url = models.URLField()
+    ready_in_minutes = models.IntegerField()
+    servings = models.IntegerField()
+    ingredients = models.TextField()  # You can store as JSON or string
+    instructions = models.TextField()
+    summary = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.user.username}"
